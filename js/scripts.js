74ec61e8d346393a3ba4dca676ceb8559f7d6c92
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-09 14:58:44 +0900

    Revise classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log ("Welcome to My Portfolio Site! Enjoy!")
 
-$(".container").mouseover(function()
+$(".overlay-container").mouseover(function()
      {
         $(".overlay").show();
      });
-$(".container").mouseout(function()
+$(".overlay-container").mouseout(function()
      {
        $(".overlay").hide();
      });
