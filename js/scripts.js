8b17522b91324d7aff89b5ae097767e6cedb4c3c
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-09 12:27:03 +0900

    Add hover event

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log ("Welcome to My Portfolio Site! Enjoy!")
+
+$(document).ready(function() {
+  $(".project-preview").hover("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project-preview").hover("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
