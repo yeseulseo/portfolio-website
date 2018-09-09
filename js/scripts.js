b0e00e32d5cd65ad3451d48002a331162deff0bd
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-09 13:08:41 +0900

    Change tags for image overlay effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log ("Welcome to My Portfolio Site! Enjoy!")
 
-$(document).ready(function() {
-  $(".project-preview").hover("mouseenter", function() {
-    $(this).find(".project-details").fadeIn();
-    });
-  $(".project-preview").hover("mouseleave", function() {
-    $(this).find(".project-details").fadeOut();
-    });
-});
+$(".container").mouseover(function()
+     {
+        $(".overlay").show();
+     });
+$(".container").mouseout(function()
+     {
+       $(".overlay").hide();
+     });
