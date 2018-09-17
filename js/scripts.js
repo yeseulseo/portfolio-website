77d0cd2bd7a9a99afbe0abb23265e6450285b876
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-18 00:57:51 +0900

    Remove memo

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
     });
 });
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -18,7 +17,6 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
