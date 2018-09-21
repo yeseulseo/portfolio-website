2336f45d5b300bb55a0b99c9c48941ba98a464b3
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-22 02:50:47 +0900

    Revise bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-  $(".overlay-container").on("mouseenter", function() {
-    $(this).find(".overlay-description").fadeIn();
-    });
-  $(".overlay-container").on("mouseleave", function() {
-    $(this).find(".overlay-description").fadeOut();
-    });
+  $('.overlay-container').on('mouseenter', function() {
+    $(this).find('.overlay-description').fadeIn();
+  });
+  $('.overlay-container').on('mouseleave', function() {
+    $(this).find('.overlay-description').fadeOut();
+  });
 });
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+    document.getElementById('myBtn').style.display = 'block';
+  } else {
+    document.getElementById('myBtn').style.display = 'none';
+  }
 }
 
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
