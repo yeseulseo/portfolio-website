a0ac2dacf33d4bf4deffa3c7dc61a4cbd4d9adfb
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-22 22:08:19 +0900

    Remove a bug

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById('myBtn').style.display = 'block';
   } else {
     document.getElementById('myBtn').style.display = 'none';
