cc83d8fd865d9efa587afa62a6cfa121820eeb15
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-23 19:56:07 +0900

    Remove extra semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,17 @@ $(document).ready(function() {
   })
 })
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() }
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('myBtn').style.display = 'block';
+    document.getElementById('myBtn').style.display = 'block'
   } else {
-    document.getElementById('myBtn').style.display = 'none';
+    document.getElementById('myBtn').style.display = 'none'
   }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0 // For Safari
+  document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
