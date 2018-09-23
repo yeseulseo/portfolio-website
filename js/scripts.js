f3ba18f21a6ba2edc31d7b5e72e818e16a8904db
YeseulSeo\Yeseul <yeseulseo24@gmail.com>
2018-09-23 20:07:38 +0900

    Add extra spaces before parentheses

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-  $('.overlay-container').on('mouseenter', function() {
+$(document).ready(function () {
+  $('.overlay-container').on('mouseenter', function () {
     $(this).find('.overlay-description').fadeIn()
   })
-  $('.overlay-container').on('mouseleave', function() {
+  $('.overlay-container').on('mouseleave', function () {
     $(this).find('.overlay-description').fadeOut()
   })
 })
 
-window.onscroll = function() { scrollFunction() }
+window.onscroll = function () { scrollFunction() }
 
-function scrollFunction() {
+function scrollFunction () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById('myBtn').style.display = 'block'
   } else {
@@ -17,7 +17,7 @@ function scrollFunction() {
   }
 }
 
-function topFunction() {
+function topFunction () {
   document.body.scrollTop = 0 // For Safari
   document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
